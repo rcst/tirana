dba506f45f750dc3f2513ac76a3f8de6b7364cd6
Eric Stemmler <rcst@posteo.de>
2021-08-17 12:52:42 +0700

    added LICENSE.md and .Rbuildignore files

---
+++ build-test.R
@@ -0,0 +1,13 @@
+rmarkdown::render(input = "inst/rmarkdown/templates/tiranatransport-presentation/skeleton/skeleton.Rmd")
+
+devtools::document()
+devtools::load_all()
+devtools::test()
+devtools::test(filter = "execute")
+devtools::build()
+devtools::check()
+
+# drat
+options(dratRepo = "/home/eric/documents/R/packages/drat/")
+drat::insertPackage(file = "../builds/rim/source/rim_0.4.1.tar.gz",  
+		      action = "archive", commit = TRUE)
